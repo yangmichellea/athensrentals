93a7dd882c3d354c672660bbb13f95f70081fc7a
yangmichellea <yangmichellea@gmail.com>
2016-08-18 16:40:17 -0400

    Initialize

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Athens Rentals</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Laravel 5</div>
+            </div>
+        </div>
+        <!-- <footer>
+            <p id="yui_3_18_1_1_1471521055480_362">© Zillow, Inc., 2006-2016. Use is subject to <a href="/corp/Terms.htm">Terms of Use</a><br><a href="/wikipages/What-is-a-Zestimate/">What's a Zestimate?</a></p>
+            <img src="http://www.zillow.com/widgets/GetVersionedResource.htm?path=/static/logos/Zillowlogo_200x50.gif" width="200" height="50" alt="Zillow Real Estate Search" id="yui_3_18_1_1_1471521055480_374">
+        </footer> -->
+    </body>
+</html>
